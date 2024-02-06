0a45ab4b678c6deb644a74e4e9990b407818650f
isaaMza <0250739@up.edu.mx>
2024-02-06 17:29:19 -0600

    progressbar3

---
+++ Eco-Race/Eco-Race/ProgressBar.swift
@@ -0,0 +1,34 @@
+//
+//  ProgressBar.swift
+//  ViewFuego
+//
+//  Created by iOS Lab on 05/02/24.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var progress:CGFloat
+    var body: some View {
+        ZStack(alignment: .leading){
+            Rectangle()
+                .frame(maxWidth: 350,maxHeight: 4)
+                .foregroundColor(Color(.gray))
+                .cornerRadius(10)
+            
+            Rectangle()
+                .frame(width: progress,height: 4)
+                .foregroundColor(Color(.red))
+                .cornerRadius(10)
+            
+        }
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar(progress: 50)
+        //aqui se cambia cada cunto se avanza de acuerdo a la pregutna
+    }
+}
+
